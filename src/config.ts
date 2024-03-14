55b22782cf43c3ef83ddb03d824568848ecbba0c
chayhan <kcw2205@naver.com>
2024-03-14 12:57:14 +0900

    remove `config.ts`
    - removed `config.ts`, it doesn't need when compiling because of changing compile options, and `config.ts` makes depths more deeper.

--- src/config.ts
+++
@@ -1,9 +0,0 @@
-import dotenv from "dotenv";
-
-dotenv.config();
-
-export const prefix = process.env.NODE_ENV === "production" ? `https://raw.githubusercontent.com/chayhan/markdown-blog/main/public` : "";
-
-export const isDebug = !(process.env.NODE_ENV === "production");
-
-export const SITE_TITLE_SUFFIX = "Chayhan log";
