af48fcf6bd2097da91143ad9f6c6b3f7b7e764f6
chayhan <kcw2205@naver.com>
2024-03-14 16:58:23 +0900

    fix target repository in build option

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 
-const repository = `Postlog`;
+const TARGET_REPOSITORY = `blog-posts`;
 
 const nextConfig = {
   output: process.env.NODE_ENV == "production" ? "export" : undefined,
   distDir: process.env.NODE_ENV == "production" ? "dist" : undefined,
   images: { unoptimized: true },
-  assetPrefix : process.env.NODE_ENV === "production" ? `/${repository}/` : "",
-  basePath : process.env.NODE_ENV === "production" ? `/${repository}` : "",
+  assetPrefix : process.env.NODE_ENV === "production" ? `/${TARGET_REPOSITORY}/` : "",
+  basePath : process.env.NODE_ENV === "production" ? `/${TARGET_REPOSITORY}` : "",
 }
 
 module.exports = nextConfig
